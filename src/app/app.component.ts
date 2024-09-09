cf412d84adddf053d3b090e186dcaaed25d7dfd0
Gabriel <gabriel10919@outlook.com>
2024-09-09 00:13:01 -0300

    Primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from "./componentes/container/container.component";
+import { CabecalhoComponent } from "./componentes/cabecalho/cabecalho.component";
+import { SeparadorComponent } from "./componentes/separador/separador.component";
+import { ContatoComponent } from './componentes/contato/contato.component';
+
+interface Contato {
+  id: number;
+  nome: string;
+  telefone: string;
+}
+
+import agenda from './agenda.json';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ContainerComponent, CabecalhoComponent, SeparadorComponent, ContatoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz';
+  contatos: Contato[] = agenda;
+
+  filtrarContatosPorLetraInicial(letra:string) : Contato[] {
+    return this.contatos.filter( contato => {
+      return contato.nome.toLowerCase().startsWith(letra)
+    } )
+  }
 }
